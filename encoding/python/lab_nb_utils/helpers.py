569c916340e19198a234c69cc80d3f4826b1e543
wabiloo <fabre.lambeau@gmail.com>
2022-04-27 10:47:28 +0100

    Allowing S3_OUTPUT_ID instead of credentials

--- encoding/python/lab_nb_utils/helpers.py
+++ encoding/python/lab_nb_utils/helpers.py
@@ -18,9 +18,12 @@ def validate_config():
         raise Exception("Module 'config' not found")
 
     assert (config.API_KEY != ''), "API_KEY is not set"
-    assert (config.S3_BUCKET_NAME != ''), "S3_BUCKET_NAME is not set"
-    assert (config.S3_ACCESS_KEY != ''), "S3_ACCESS_KEY is not set"
-    assert (config.S3_SECRET_KEY != ''), "S3_SECRET_KEY is not set"
+
+    if not getattr(config, 'S3_OUTPUT_ID'):
+        assert (config.S3_BUCKET_NAME != ''), "S3_BUCKET_NAME is not set"
+        assert (config.S3_ACCESS_KEY != ''), "S3_ACCESS_KEY is not set"
+        assert (config.S3_SECRET_KEY != ''), "S3_SECRET_KEY is not set"
+
     assert (config.MY_ID != ''), "MY_ID is not set"
 
     return "Your configuration appears complete"
