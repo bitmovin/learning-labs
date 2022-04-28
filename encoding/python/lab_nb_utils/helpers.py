8735abc0c0674748a50af26a9b4e7db5a62a9dda
wabiloo <fabre.lambeau@gmail.com>
2022-04-28 13:11:12 +0100

    Change to config var names

--- encoding/python/lab_nb_utils/helpers.py
+++ encoding/python/lab_nb_utils/helpers.py
@@ -19,18 +19,18 @@ def validate_config():
 
     assert (config.API_KEY != ''), "API_KEY is not set"
 
-    if not getattr(config, 'S3_OUTPUT_ID'):
+    if not getattr(config, 'OUTPUT_ID'):
         assert (config.S3_BUCKET_NAME != ''), "S3_BUCKET_NAME is not set"
         assert (config.S3_ACCESS_KEY != ''), "S3_ACCESS_KEY is not set"
         assert (config.S3_SECRET_KEY != ''), "S3_SECRET_KEY is not set"
 
-    assert (config.MY_ID != ''), "MY_ID is not set"
+    assert (config.MY_LABEL != ''), "MY_LABEL is not set"
 
     return "Your configuration appears complete"
 
 
 def build_output_path():
-    return f"outputs/{config.MY_ID}-{get_uuid()}"
+    return f"outputs/{config.MY_LABEL}-{get_uuid()}"
 
 
 def build_dashboard_url(encoding_id):
