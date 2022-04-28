b081ed7f6fa8634fecb72c20619663d15d759fa7
wabiloo <fabre.lambeau@gmail.com>
2022-04-28 11:48:57 +0100

    improvement to notebook display helpers

--- encoding/python/lab_nb_utils/nprint.py
+++ encoding/python/lab_nb_utils/nprint.py
@@ -1,17 +1,28 @@
 from IPython.display import Markdown, display
 
 
-def info(msg):
-    display(Markdown(f"**{msg}**"))
+def _build_msg(msg, code=None, color=None, bold=True):
+    out = msg
+    if code:
+        out += f" <code>{code}</code>"
+    if bold:
+        out = f"<b>{out}</b>"
+    if color:
+        out = f"<font color='{color}'>{out}</font>"
+    return Markdown(out)
 
 
-def ok(msg):
-    display(Markdown(
-        f"<font color='green'>{msg}</font>"
-    ))
+def info(msg, val=None):
+    display(_build_msg(msg, val, None, True))
 
 
-def error(msg):
-    display(Markdown(
-        f"<font color='red'>{msg}</font>"
-    ))
+def ok(msg, val=None):
+    display(_build_msg(msg, val, 'green', True))
+
+
+def error(msg, val=None):
+    display(_build_msg(msg, val, 'red', True))
+
+
+def debug(msg, val=None):
+    display(_build_msg(msg, val, 'gray', None))
