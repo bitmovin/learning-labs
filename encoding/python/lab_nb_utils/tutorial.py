7a768047bb6e9bce7e64ccbb2c3eada8bd1be8ed
wabiloo <fabre.lambeau@gmail.com>
2022-05-20 22:59:10 +0100

    get_dashboard_url

--- encoding/python/lab_nb_utils/tutorial.py
+++ encoding/python/lab_nb_utils/tutorial.py
@@ -143,6 +143,9 @@ class TutorialHelper:
         output_path = f"outputs/{config.MY_LABEL}-{str(uuid.uuid4())[:8]}"
         return output_path
 
+    def get_dashboard_url(encoding_id):
+        return f"https://bitmovin.com/dashboard/encoding/encodings/{encoding_id}?apiKey={config.API_KEY}&orgId={config.ORG_ID}"
+
 
 def camelize(string):
     if not string.isalnum():
