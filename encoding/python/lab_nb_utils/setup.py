80b284dea9283d4ce342ba1c98033bc507e348c9
wabiloo <fabre.lambeau@gmail.com>
2022-05-23 12:12:34 +0100

    Tests with requirements and local modules

---
+++ encoding/python/lab_nb_utils/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='encoding-learning-labs',
+    version='0.3',
+    packages=['tests'],
+    package_dir={'': 'encoding/python/lab_nb_utils'},
+    url='',
+    license='',
+    author='fabre.lambeau',
+    author_email='fabe.lambeau@bitmovin.com',
+    description='Helpers for Bitmovin Encoding tutorials'
+)
