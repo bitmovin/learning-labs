968e1abfd9cdd832e4767545ba7ad34509f8c533
wabiloo <fabre.lambeau@gmail.com>
2022-05-23 13:17:53 +0100

    Fixing urllib3 incompatibility

--- encoding/python/setup.py
+++ encoding/python/setup.py
@@ -4,7 +4,10 @@ setup(
     name='encoding-learning-labs',
     version='0.3',
     packages=['lab_nb_utils'],
-    # package_dir={'': 'lab_nb_utils'},
+    install_requires=[
+        'bitmovin-api-sdk',
+        'boto3==1.12.1'   # OLD version to ensure compatibility of urllib3 with the one used by bitmovin-api-sdk
+    ],
     url='',
     license='',
     author='fabre.lambeau',
